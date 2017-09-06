b69cf1ba3bd9bf16d3372aa7bf21d8ee66389b15
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-09-05 19:15:16 -0700

    perltidy lib/Importer/Zim/Base.pm

--- lib/Importer/Zim/Base.pm
+++ lib/Importer/Zim/Base.pm
@@ -2,26 +2,26 @@
 package Importer::Zim::Base;
 
 use 5.018;
-use Carp ();
+use Carp            ();
 use Module::Runtime ();
 
 sub _prepare_args {
-    my $class = shift;
+    my $class   = shift;
     my $package = shift
-       or Carp::croak qq{Usage: use $class MODULE => EXPORTS...\n};
+      or Carp::croak qq{Usage: use $class MODULE => EXPORTS...\n};
     Module::Runtime::require_module($package);
 
     my @exports;
     while (@_) {
         my $symbol = shift;
-        my $opts = ref $_[0] ? shift : { -as => $symbol };
+        my $opts   = ref $_[0] ? shift : { -as => $symbol };
         my $export = $opts->{-as};
-        my $sub = do {
+        my $sub    = do {
             no strict 'refs';
             *{"${package}::${symbol}"}{CODE};
         };
         Carp::croak qq{Can't find "$symbol" in "$package"}
-            unless $sub;
+          unless $sub;
         push @exports, { export => $export, code => $sub };
     }
     return @exports;
